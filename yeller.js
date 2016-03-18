bad8039dc193b003b2ed998c87ce7163a3d66a18
TreverH <treverh@qualtrics.com>
2016-03-18 10:55:59 -0600

    Add date time string and switched things up so you can sandbox custom statements if needed

--- yeller.js
+++ yeller.js
@@ -6,53 +6,32 @@ module.exports.colors = {
 	RESET:"\x1b[0m"
 };
 
+var colors = module.exports.colors;
+
 module.exports.custom = function(color,heading,msg,showLineInfo){
-	if(!color){
-		loggerError("A valid color is required!");
-		return;
-	}
-	if(!heading){
-		loggerError("'heading' is required!");
-		return;
-	}
-	var msgInfo = "";
-	if(msg){
-		msgInfo += ": " + this.colors.RESET + convertMessage(msg);
-	}
-	else{
-		msgInfo += this.colors.RESET;
-	}
-	var lineInfo = "";
-	if(showLineInfo){
-		var info = getInfo(1);
-		if(info.file.indexOf('/yeller/yeller.js') >= 0){
-			info = getInfo(2);
-		}
-		lineInfo += this.colors.CYAN + " (" + info.file + ":" + info.line + ")";
-	}
-	console.log(color + heading + lineInfo + msgInfo);
+	printMessage(color,heading,msg,showLineInfo);
 }
 
 module.exports.info = function(msg){
-	this.custom(this.colors.GREEN,"INFO",msg,true);
+	printMessage(colors.GREEN,"INFO",msg,true);
 }
 
 module.exports.warn = function(msg){
-	this.custom(this.colors.YELLOW,"WARN",msg,true);
+	printMessage(colors.YELLOW,"WARN",msg,true);
 }
 
 module.exports.error = function(msg){
-	this.custom(this.colors.RED,"ERROR",msg,true);
+	printMessage(colors.RED,"ERROR",msg,true);
 }
 
 module.exports.debug = function(msg){
-	this.custom(this.colors.RED,"DEBUG",msg,true);
+	printMessage(colors.RED,"DEBUG",msg,true);
 }
 
 var loggerError = function(msg){
 	var info = getInfo(1);
-	var lineInfo = module.exports.colors.CYAN + " (" + info.file + ":" + info.line + ")";
-	console.log(module.exports.colors.RED + "LOGGER ERROR" + lineInfo + ": " + module.exports.colors.RESET + msg);
+	var lineInfo = colors.CYAN + " (" + info.file + ":" + info.line + ")";
+	console.log(colors.RED + "LOGGER ERROR" + lineInfo + ": " + colors.RESET + msg);
 }
 
 function convertMessage(msg){
@@ -73,4 +52,33 @@ function getInfo(lineNumber){
 		line:stack[lineNumber].getLineNumber(),
 		file:stack[lineNumber].getFileName()
 	}
+}
+
+function printMessage(color,heading,msg,showLineInfo){
+	if(!color){
+		loggerError("A valid color is required!");
+		return;
+	}
+	if(!heading){
+		loggerError("'heading' is required!");
+		return;
+	}
+	var msgInfo = "";
+	if(msg){
+		msgInfo += ": " + colors.RESET + convertMessage(msg);
+	}
+	else{
+		msgInfo += colors.RESET;
+	}
+	var lineInfo = "";
+	if(showLineInfo){
+		var date = new Date();
+		var dateString = date.getMonth() + "/" + date.getDay() + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+		var info = getInfo(1);
+		if(info.file.indexOf('/yeller/yeller.js') >= 0){
+			info = getInfo(2);
+		}
+		lineInfo += colors.CYAN + " (" + info.file + ":" + info.line + " - " + dateString + ")";
+	}
+	console.log(color + heading + lineInfo + msgInfo);
 }
