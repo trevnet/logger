57cc5d8726252921f1e846cacdc3fe2e34c92751
Ammon S <ammons@qualtrics.com>
2016-03-29 19:02:48 -0600

    ReadME and rearrange test

--- tests/test.js
+++ tests/test.js
@@ -1,15 +1,43 @@
 var yeller = require("../yeller");
 
-yeller.custom(yeller.colors.CYAN,"CUSTOM","This is a custom message",false)
-yeller.custom(yeller.colors.CYAN,"CUSTOM","This is a custom message with line info",true)
-yeller.custom(yeller.colors.RED,"****CUSTOM HEADING AND NOTHING ELSE****")
-yeller.custom(yeller.colors.UNKNOWNCOLOR,"INCORRECT COLOR")
-console.log("This is here to show the color gets reset correctly")
-yeller.info("This is some info")
-yeller.warn("This is a warning")
+//Log Levels
+yeller.emergency("This is an emergency")
+yeller.alert("This is an alert")
+yeller.critical("This is an critical error")
 yeller.error("This is an error")
+yeller.warn("This is a warning")
+yeller.notice("This is a notice")
+yeller.info("This is some info")
 yeller.debug("This is a debug statement")
-yeller.debug({"This is":"an object I'd like","to see":"pretty printed","with":['nested arrays',{"and":"objects"}]})
+
+//Custom yells with an object
+yeller.log({msg:"Hi There"}) //defaults to green heading of LOG and shows line info
+yeller.log({
+	color:yeller.colors.RED+yeller.colors.BOLD,
+	heading:"GO AWAY",
+	msg:"you are not welcome here",
+	showLineInfo:false
+})
+yeller.log({
+	color:yeller.colors.NOCOLOR,
+	heading:"I'm a bland message", //Use header when doing one liners
+	showLineInfo:true
+})
+
+//Pretty Prints Objects
+yeller.debug({"This is":"an object I'd like","to see":"pretty printed","with":['nested arrays',{"and":"objects",its:true},"I'm too cool",4,null]})
+yeller.info(["This is",null,{an:"array",with:"showLineInfo",setTo:false},4, "example"],false);
+
+//Logs functions as strings
 yeller.debug(function(something){ return something; })
 yeller.debug(yeller.debug)
-yeller.custom(yeller.colors.RED)
+
+//Formatting Options (BOLD,BLINK,INVERTED,UNDERLINED,BACKGROUND COLORS,ETC)
+yeller.custom(yeller.colors.BGGRAY+yeller.colors.RED+yeller.colors.UNDERLINED,"Not Bold Heading","Not Bold",true) //gray background, red underlined text
+yeller.custom(yeller.colors.BOLD+yeller.colors.BLINK+yeller.colors.LTRED,"Bold Heading","Bold",true) //bold, blinking, light red text (Note Blink only works on certain terminals)
+
+//Custom Yells (Depricated)
+yeller.custom(yeller.colors.CYAN,"CUSTOM","This is a custom message without line info",false)
+yeller.custom(yeller.colors.CYAN,"CustomHeading","This is a custom message with line info",true)
+yeller.custom(yeller.colors.RED,"****CUSTOM HEADING AND NOTHING ELSE****")
+yeller.custom(yeller.colors.UNKNOWNCOLOR,"INCORRECT COLOR") // throws error expects color from yeller.colors
--- yeller.js
+++ yeller.js
@@ -1,43 +1,113 @@
 module.exports.colors = {
+	RESET:"\x1b[0m",
+	NOCOLOR:"\x1b[0m",
+	BOLD:"\x1b[1m",
+	DIM:"\x1b[2m",
+	UNDERLINED:"\x1b[4m",
+	BLINK:"\x1b[5m",
+	REVERSE:"\x1b[7m",
+	HIDDEN:"\x1b[8m",
+	BLACK:"\x1b[30m",
 	RED:"\x1b[31m",
 	GREEN:"\x1b[32m",
 	YELLOW:"\x1b[33m",
+	BLUE:"\x1b[34m",
+	MAGENTA:"\x1b[35m",
 	CYAN:"\x1b[36m",
-	BLUE:"\x1b[94m",
-	RESET:"\x1b[0m"
+	LTGRAY:"\x1b[37m",
+	GRAY:"\x1b[90m",
+	LTRED:"\x1b[91m",
+	LTGREEN:"\x1b[92m",
+	LTYELLOW:"\x1b[93m",
+	LTBLUE:"\x1b[94m",
+	LTMAGENTA:"\x1b[95m",
+	LTCYAN:"\x1b[96m",
+	WHITE:"\x1b[97m",
+	BGBLACK:"\x1b[40m",
+	BGRED:"\x1b[41m",
+	BGGREEN:"\x1b[42m",
+	BGYELLOW:"\x1b[43m",
+	BGBLUE:"\x1b[44m",
+	BGMAGENTA:"\x1b[45m",
+	BGCYAN:"\x1b[46m",
+	BGLTGRAY:"\x1b[47m",
+	BGGRAY:"\x1b[100m",
+	BGLTRED:"\x1b[101m",
+	BGLTGREEN:"\x1b[102m",
+	BGLTYELLOW:"\x1b[103m",
+	BGLTBLUE:"\x1b[104m",
+	BGLTMAGENTA:"\x1b[105m",
+	BGLTCYAN:"\x1b[106m",
+	BGWHITE:"\x1b[107m"
 };
 
-var colors = module.exports.colors;
+var c = module.exports.colors;
+
+module.exports.emergency=function(msg,showLineInfo){
+	printMessage(c.BLINK+c.BGLTRED+c.BOLD+c.WHITE," EMERGENCY ",msg, typeof showLineInfo === 'boolean'?showLineInfo:true);
+}
+module.exports.alert=function(msg,showLineInfo){
+	printMessage(c.BGRED+c.BOLD+c.LTGRAY," ALERT ",msg, typeof showLineInfo === 'boolean'?showLineInfo:true);
+}
+module.exports.critical=function(msg,showLineInfo){
+	printMessage(c.BOLD+c.LTRED,"CRITICAL",msg, typeof showLineInfo === 'boolean'?showLineInfo:true);
+}
+module.exports.error=function(msg,showLineInfo){
+	printMessage(c.RED+c.BOLD,"ERROR",msg, typeof showLineInfo === 'boolean'?showLineInfo:true);
+}
+module.exports.warn=function(msg,showLineInfo){
+	printMessage(c.BGGRAY+c.BOLD+c.LTYELLOW," WARNING ",msg, typeof showLineInfo === 'boolean'?showLineInfo:true);
+}
+module.exports.notice=function(msg,showLineInfo){
+	printMessage(c.BGBLUE+c.BOLD+c.WHITE," NOTICE ",msg, typeof showLineInfo === 'boolean'?showLineInfo:true);
+}
+module.exports.info=function(msg,showLineInfo){
+	printMessage(c.GREEN+c.BOLD,"INFO",msg, typeof showLineInfo === 'boolean'?showLineInfo:true);
+}
+module.exports.debug=function(msg,showLineInfo){
+	printMessage(c.BLUE+c.BOLD+c.BGLTGRAY," DEBUG ",msg, typeof showLineInfo === 'boolean'?showLineInfo:true);
+}
 
 module.exports.custom = function(color,heading,msg,showLineInfo){
 	printMessage(color,heading,msg,showLineInfo);
 }
 
-module.exports.info = function(msg){
-	printMessage(colors.GREEN,"INFO",msg,true);
-}
-
-module.exports.warn = function(msg){
-	printMessage(colors.YELLOW,"WARN",msg,true);
-}
-
-module.exports.error = function(msg){
-	printMessage(colors.RED,"ERROR",msg,true);
-}
-
-module.exports.debug = function(msg){
-	printMessage(colors.BLUE,"DEBUG",msg,true);
+module.exports.log = function(obj){
+	printMessage(
+		obj.color ? c[obj.color] : c.GREEN,
+		obj.heading || "LOG",
+		obj.msg || "",
+		obj.showLineInfo || true
+	);
 }
 
 var loggerError = function(msg){
 	var info = getInfo(1);
-	var lineInfo = colors.CYAN + " (" + info.file + ":" + info.line + ")";
-	console.log(colors.RED + "LOGGER ERROR" + lineInfo + ": " + colors.RESET + msg);
+	var lineInfo = c.CYAN + " (" + info.file + ":" + info.line + ")";
+	console.log(c.RED + "LOGGER ERROR" + lineInfo + ": " + c.RESET + msg);
 }
 
 function convertMessage(msg){
 	if(typeof msg == 'object'){
-		msg = colors.YELLOW+"Object:\n"+colors.RESET+ JSON.stringify(msg,null,'  ');
+		msg = JSON.stringify(msg,null,'\t');
+		msg = msg.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+				var cls = 'CYAN';
+				if (/^"/.test(match)) {
+						if (/:$/.test(match)) {
+								cls = 'RED'; //KEY
+						} else {
+								cls = 'GREEN'; //STRING
+						}
+				} else if (/true|false/.test(match)) {
+						cls = 'YELLOW'; //BOOLEAN
+				} else if (/null|undefined/.test(match)) {
+						cls = 'GRAY'; //NULL
+				}
+
+				return c[cls] + match + c.RESET;
+		});
+		msg = msg.replace(/\n/g,"\n\t");
+		msg = c.YELLOW + "OBJECT:\n\t" + c.RESET + msg;
 	}
 	return msg;
 }
@@ -66,10 +136,10 @@ function printMessage(color,heading,msg,showLineInfo){
 	}
 	var msgInfo = "";
 	if(msg){
-		msgInfo += ": " + colors.RESET + convertMessage(msg);
+		msgInfo += ": " + c.RESET + convertMessage(msg);
 	}
 	else{
-		msgInfo += colors.RESET;
+		msgInfo += c.RESET;
 	}
 	var lineInfo = "";
 	if(showLineInfo){
@@ -79,7 +149,7 @@ function printMessage(color,heading,msg,showLineInfo){
 		if(info.file.indexOf('/yeller/yeller.js') >= 0){
 			info = getInfo(2);
 		}
-		lineInfo += colors.CYAN + " (" + info.file + ":" + info.line + " - " + dateString + ")";
+		lineInfo += c.RESET + c.CYAN + " (" + info.file + ":" + info.line + " - " + dateString + ")";
 	}
 	console.log(color + heading + lineInfo + msgInfo);
 }
